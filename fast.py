ce85fbb426a5a1ae3cb28dcc4275be06697304e1
Trivium0911 <triv_1993@mail.ru>
2021-08-17 21:21:30 +0300

    Change fastapi #2

--- fast.py
+++ fast.py
@@ -15,13 +15,17 @@ class Numbers(BaseModel):
 class TaskArgs(BaseModel):
     name: str
     args: Numbers
-
+numbers = []
 @app.post("/my/")
 async def handler(obj: TaskArgs):
     a = obj.args.a
     b = obj.args.b
     return {"result" : int(math.sqrt(a) + b), "task" : obj.name}
 @app.post("/task/4")
-async def handler(number: int = Body(...)):
-    print(f"new number: {number}")
-    return number
+async def handler(data: str = Body(...)):
+    if data == "stop":
+        return sum(numbers)
+    else:
+        assert data.isdigit()
+        numbers.append(int(data))
+        return numbers
