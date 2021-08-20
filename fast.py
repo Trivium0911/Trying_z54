12df8aafe42a2bf875c436defb88890e28de93cd
Trivium0911 <triv_1993@mail.ru>
2021-08-20 10:19:18 +0300

    Fixed little bug in fast.py

--- fast.py
+++ fast.py
@@ -27,7 +27,7 @@ async def handler(obj: TaskArgs):
 
 numbers = defaultdict(list)
 def gen_random_name():
-    return os.urandom(16),hex()
+    return os.urandom(16).hex()
 
 def get_user(request: Request):
     return request.cookies.get("user")
